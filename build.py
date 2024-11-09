9a85c613c56228e22c46a4b3a1e1800e8014bf4e
nyxical420 <xaveegacha7@gmail.com>
2024-11-09 21:37:23 +0800

    build script

---
+++ build.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import subprocess
+
+pyinstaller_command = [
+    'pyinstaller',
+    '--onefile',
+    '--noconsole',
+    '--icon=./assets/rodnmod.ico',
+    'main.py'
+]
+
+subprocess.run(pyinstaller_command)
+
+if os.path.exists('./dist/main.exe'):
+    shutil.move('./dist/main.exe', './rodnmod.exe')
+
+remove = [
+    "./build",
+    "./dist",
+    "./main.spec",
+]
+
+for item in remove:
+    if os.path.exists(item):
+        if os.path.isdir(item):
+            shutil.rmtree(item)
+        else:
+            os.remove(item)
+
+print(f"Build complete.")
